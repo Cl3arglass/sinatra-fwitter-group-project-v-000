ae4b6973aa246bda4fc2b8c0f28e186ba4a1c459
Holden Altaffer <haltaffer@gmail.com>
2018-07-09 23:28:49 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  
-  get "/users/#{user.slug}" do 
+
+  get "/users/#{user.slug}" do
+    @user = User.find_by_slug(params[:slug])
+
+    erb :'users/show'
   end
 end
